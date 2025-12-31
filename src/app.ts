ff8fe8884fcffae82cc658fdfbd3aec46e891c9c
mchamoudadev <mchamuuda@gmail.com>
2025-12-31 20:10:25 +0300

    see if we are in production

--- src/app.ts
+++ src/app.ts
@@ -77,7 +77,7 @@ app.get('/health', apiRateLimitConfig.general, (req, res, next) => {
     res.json({
         status: "ok",
         timeStamp: new Date().toISOString(),
-        message: "Backend is running with PM2 Process Manager and Node.js version 24.x 😍",
+        message: "Backend is running with PM2 Process Manager and Node.js version 24.x 😍 ${config.env}",
         frontendUrl: config.frontendUrl,
         env: config.env,
     })
