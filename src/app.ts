f35edf9d171a564feb58af2449ea8dfbc891a288
mchamoudadev <mchamuuda@gmail.com>
2025-12-31 19:38:44 +0300

    health route update

--- src/app.ts
+++ src/app.ts
@@ -77,7 +77,7 @@ app.get('/health', apiRateLimitConfig.general, (req, res, next) => {
     res.json({
         status: "ok",
         timeStamp: new Date().toISOString(),
-        message: "Backend is running",
+        message: "Backend is running with PM2 Process Manager and Node.js version 24.x 😍",
     })
 })
 
